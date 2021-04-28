074479a6cf2490e5e02b0823fb0a9d6a0f451d80
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-04-28 14:49:21 +0900

    title \ Stack 함수 구현 완료

--- Java/Algorithm/src/stack/StackExample.java
+++ Java/Algorithm/src/stack/StackExample.java
@@ -7,10 +7,11 @@ public class StackExample
     public static void main(String [] args)
     {
         Stack<String> stack = new Stack<>();
-        System.out.println(stack.empty());
+        MyStack myStack = new MyStack();
         for (int i = 1; i <= 5; i++)
         {
             stack.push(String.valueOf(i));
+            myStack.push(String.valueOf(i));
         }
         System.out.println(stack.empty());
         System.out.println(stack.peek());
@@ -19,5 +20,20 @@ public class StackExample
         System.out.println(stack);
         System.out.println(stack.search("6"));
         System.out.println(stack.search("1"));
+        stack.clear();
+        System.out.println(stack);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(myStack.empty());
+        System.out.println(myStack.peek());
+        System.out.println(myStack);
+        System.out.println(myStack.pop());
+        System.out.println(myStack);
+        System.out.println(myStack.search("6"));
+        System.out.println(myStack.search("1"));
+        myStack.clear();
+        System.out.println(myStack);
     }
 }
