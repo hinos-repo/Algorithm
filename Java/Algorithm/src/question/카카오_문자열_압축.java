51d6a8ee4e40425bb94d2bd113b39ddb42607fa1
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-05-07 13:01:24 +0900

    title \ 카카오 문자열 압축 알고리즘 풀이 from \ 프로그래머스

---
+++ Java/Algorithm/src/question/카카오_문자열_압축.java
@@ -0,0 +1,78 @@
+package question;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class 카카오_문자열_압축
+{
+    // aabbcaabb
+
+    public static void main(String [] args)
+    {
+        HashMap<String, Integer> testData = new HashMap<>();
+        // 2a2bac2c
+        // ac2c
+        testData.put("aabbaccc", 7);
+        testData.put("ababcdcdababcdcd", 9);
+        testData.put("abcabcdede", 8);
+        testData.put("abcabcabcabcdededededede", 14);
+        testData.put("xababcdcdababcdcd", 17);
+
+
+        Iterator iterator = testData.keySet().iterator();
+        while (iterator.hasNext())
+        {
+            String key = (String)iterator.next();
+            System.out.println(key + ":" + ((solution(key) == testData.get(key)) ? "성공" : "실패") );
+        }
+    }
+
+    static int solution(String s)
+    {
+        if (s.length() == 1)
+            return 1;
+
+        int resultCount = 1000;
+        for (int i = 1; i < (s.length() / 2) + 1; i++)
+        {
+            String result = "";
+            String prev = "";
+            int count = 1;
+            for(int j = 0; j < (s.length() / i) + 1; j++)
+            {
+                int start = j * i;
+                int end = start+i;
+                if (end > s.length())
+                {
+                    end = s.length();
+                }
+
+                String current = s.substring(start, end);
+                if (prev.equals(current) && !prev.equals(""))
+                {
+                    count++;
+                    continue;
+                }
+                prev = current;
+                result += parseString(count, prev);
+                count = 1;
+            }
+            System.out.println(i + "묶음 : " + result);
+            if (resultCount > result.length())
+            {
+                resultCount = result.length();
+            }
+
+        }
+        return resultCount;
+    }
+
+    static String parseString(int count, String str)
+    {
+        if (count <= 1)
+        {
+            return str;
+        }
+        return count+str;
+    }
+}
