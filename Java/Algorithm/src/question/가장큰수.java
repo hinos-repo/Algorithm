f4a1ebc9bd7ebabcde9eb006a145ff8f87373ac0
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-05-17 11:32:10 +0900

    title \ 가장큰수 풀이 from \ 프로그래머스

--- Java/Algorithm/src/question/가장큰수.java
+++ Java/Algorithm/src/question/가장큰수.java
@@ -1,7 +1,7 @@
 package question;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 
 public class 가장큰수
 {
@@ -9,11 +9,40 @@ public class 가장큰수
     {
         int[] numbers = new int[]{3, 30, 34, 5, 9};
 //        System.out.println(toString(numbers));
-        System.out.println(solution(numbers));
+        System.out.println(solution2(numbers));
     }
 
+    public static String solution2(int[] numbers) //정답
+    {
+        // 숫자를 문자열로 변환
+        String[] result = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = String.valueOf(numbers[i]);
+        }
 
-    public static String solution(int[] numbers)
+        Arrays.sort(result, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if (result[0].equals("0"))
+        {
+            return "0";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (String element : result)
+        {
+            builder.append(element);
+        }
+
+        String answer = new String(builder);
+        return answer.equals("") ? "0" : new String(builder);
+    }
+
+    public static String solution(int[] numbers) // 오답
     {
         String answer = "";
         sortDesc(numbers);
@@ -64,6 +93,22 @@ public class 가장큰수
     }
 
     private static String toString(int [] result)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (int i = 0; i<result.length; i++)
+        {
+            builder.append(result[i]);
+            if (i < result.length-1)
+            {
+                builder.append(",");
+            }
+        }
+        builder.append("]");
+        return new String(builder);
+    }
+
+    private static String toString2(String [] result)
     {
         StringBuilder builder = new StringBuilder();
         builder.append("[");
