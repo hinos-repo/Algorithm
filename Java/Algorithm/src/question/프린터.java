52237379dfb6e6d56b06505130c2ef1642a62255
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-05-18 11:53:32 +0900

    title \ 프린터 알고리즘 풀이 from \ 프로그래머스

---
+++ Java/Algorithm/src/question/프린터.java
@@ -0,0 +1,61 @@
+package question;
+
+import java.util.LinkedList;
+
+public class 프린터
+{
+
+    public static void main(String [] args)
+    {
+        int [] prio = new int[] {1, 1, 9, 1, 1, 1};
+        int location = 0;
+
+        System.out.println(solution(prio, location));
+    }
+
+    public static int solution(int[] priorities, int location)
+    {
+        LinkedList<Integer> arrData = new LinkedList<>();
+        for (int element : priorities)
+        {
+            arrData.add(element);
+        }
+
+        int printCount = 0;
+        while (-1 < location)
+        {
+            int first = arrData.get(0);
+            boolean flag = false; // 뒤에 큰 수가 있으면 true
+            for (int j = 1; j < arrData.size(); j++)
+            {
+                if (first < arrData.get(j))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                if (location == 0)
+                {
+                    location = arrData.size() -1;
+                } else {
+                    location--;
+                }
+                arrData.removeFirst();
+                arrData.addLast(first);
+            } else { // 큰 수가 없음
+                printCount++;
+                if (location == 0)
+                {
+                    return printCount;
+                } else {
+                    arrData.removeFirst();
+                    location--;
+                }
+            }
+        }
+        return printCount;
+    }
+}
