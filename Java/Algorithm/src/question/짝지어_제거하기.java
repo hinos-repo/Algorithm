1399d08ea6b5ee701908e3956f6a8124b224da54
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-05-26 11:49:26 +0900

    title \ 짝지어 제거하기 from \ 프로그래머스

---
+++ Java/Algorithm/src/question/짝지어_제거하기.java
@@ -0,0 +1,53 @@
+package question;
+
+import java.util.*;
+public class 짝지어_제거하기
+{
+    public static void main(String[] args)
+    {
+        System.out.println(solution1("baabaa"));
+        System.out.println(solution1("cdcd"));
+    }
+
+    private static int solution1(String s) //효율성 통과X
+    {
+        ArrayList<Character> arr = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            arr.add(s.charAt(i));
+        }
+
+        for (int i = arr.size()-1; i > 0; i--)
+        {
+            if (arr.get(i).equals(arr.get(i-1)))
+            {
+                arr.remove(i);
+                arr.remove(i-1);
+                i = Math.min(i+2, arr.size());
+            }
+        }
+
+        return arr.isEmpty() ? 1 : 0;
+    }
+
+    private static int solution2(String s) //효율성 통과
+    {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (stack.isEmpty())
+            {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.peek() == s.charAt(i))
+                {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
