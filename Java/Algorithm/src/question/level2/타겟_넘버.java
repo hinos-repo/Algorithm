f8480576eed24b4eb65bc7e07c7fb5d17903bb55
hinos-repo <60995477+hinos-repo@users.noreply.github.com>
2021-06-14 11:51:03 +0900

    title \ 타겟 넘버 값 직접 입력

--- Java/Algorithm/src/question/level2/타겟_넘버.java
+++ Java/Algorithm/src/question/level2/타겟_넘버.java
@@ -1,29 +1,39 @@
 package question.level2;
 
-import java.util.ArrayList;
+import test.MyUtil;
 
 public class 타겟_넘버
 {
     public static void main(String[] args)
     {
-        int [] numbers = new int[] {1, 1, 1, 1, 1};
+        int [] numbers = new int[] {1, 1, 1};
         int target = 3;
 
-        System.out.println(solution(numbers, target));
+        System.out.println(solution1(numbers, target));
+        System.out.println("------------------------");
+        System.out.println(solution2(numbers, target));
     }
 
-    static int answer = 0;
-
-    public static int solution(int[] numbers, int target)
+    static int answer1 = 0;
+    static int answer2 = 0;
+    public static int solution1(int[] numbers, int target)
     {
-        dfs(numbers, target, 0);
-        return answer;
+        dfs1(numbers, target, 0);
+        return answer1;
     }
 
-    public static void dfs(int[] numbers, int target, int node)
+    public static int solution2(int[] numbers, int target)
+    {
+        dfs2(numbers, target, 0);
+        return answer2;
+    }
+
+
+    public static void dfs1(int[] numbers, int target, int node)
     {
         if (node == numbers.length)
         {
+            System.out.println(MyUtil.toString(numbers));
             int sum = 0;
             for (int num : numbers)
             {
@@ -31,15 +41,39 @@ public class 타겟_넘버
             }
             if (sum == target)
             {
-                answer++;
+                answer1++;
             }
         }
         else {
             numbers[node] *= 1;
-            dfs(numbers, target, node + 1);
+            dfs1(numbers, target, node + 1);
 
             numbers[node] *= -1;
-            dfs(numbers, target, node + 1);
+            dfs1(numbers, target, node + 1);
+        }
+    }
+
+    public static void dfs2(int[] numbers, int target, int node)
+    {
+        if (node == numbers.length)
+        {
+            System.out.println(MyUtil.toString(numbers));
+            int sum = 0;
+            for (int num : numbers)
+            {
+                sum += num;
+            }
+            if (sum == target)
+            {
+                answer2++;
+            }
+        }
+        else {
+            numbers[node] = 1;
+            dfs2(numbers, target, node + 1);
+
+            numbers[node] = -1;
+            dfs2(numbers, target, node + 1);
         }
     }
 }
